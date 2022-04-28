2e4923a4d4c47745ecc9537a73fef12832814cdf
Mᴀʀsʜᴍᴇʟʟᴏᴡ Fᴢ Tᴇᴀᴍ <93227443+Ndiproject@users.noreply.github.com>
2022-04-28 18:45:25 +0700

    Update Admins.py

--- Yukki/Plugins/Admins.py
+++ Yukki/Plugins/Admins.py
@@ -36,14 +36,10 @@ __MODULE__ = "Admin"
 __HELP__ = """
 
 
-/pause - Jeda musik yang diputar di obrolan suara.
-
-/resume - Melanjutkan musik yang dijeda di obrolan suara.
-
-/skip - Lewati musik yang sedang diputar di obrolan suara
-
-/stop - Memberhentikan Musik
-
+/pause - Jeda pemutaran musik di obrolan suara.
+/resume - Lanjutkan musik yang dijeda di obrolan suara.
+/skip - Lewati musik yang sedang diputar di obrolan suara.
+/stop - Hentikan permainannya.
 /queue - Periksa daftar antrian.
 """
 
@@ -57,27 +53,27 @@ __HELP__ = """
 async def admins(_, message: Message):
     global get_queue
     if not len(message.command) == 1:
-        return await message.reply_text("Error! Wrong Usage of Command.")
+        return await message.reply_text("Kesalahan! Penggunaan Perintah yang Salah.")
     if not await is_active_chat(message.chat.id):
         return await message.reply_text(
-            "Nothing is playing on voice chat. No Active Voice Chat Found"
+            "Tidak ada yang diputar di obrolan suara. Obrolan Suara Aktif Tidak Ditemukan"
         )
     chat_id = message.chat.id
     if message.command[0][1] == "a":
         if not await is_music_playing(message.chat.id):
-            return await message.reply_text("Music is already Paused.")
+            return await message.reply_text("Musik sudah Dijeda.")
         await music_off(chat_id)
         await pause_stream(chat_id)
         await message.reply_text(
-            f"🎧 Obrolan Suara Dijeda oleh {message.from_user.mention}!"
+            f"⏸️ Obrolan Suara Dijeda oleh {message.from_user.mention}!"
         )
     if message.command[0][1] == "e":
         if await is_music_playing(message.chat.id):
-            return await message.reply_text("Music is already Playing.")
+            return await message.reply_text("Musik sudah Diputar.")
         await music_on(chat_id)
         await resume_stream(chat_id)
         await message.reply_text(
-            f"🎧 Obrolan Suara Dilanjutkan oleh {message.from_user.mention}!"
+            f"▶️ Obrolan Suara Dilanjutkan oleh {message.from_user.mention}!"
         )
     if message.command[0][1] == "t" or message.command[0][1] == "n":
         if message.chat.id not in db_mem:
@@ -92,7 +88,7 @@ async def admins(_, message: Message):
         await remove_active_video_chat(chat_id)
         await stop_stream(chat_id)
         await message.reply_text(
-            f"🎧 Obrolan Suara Berakhir/Dihentikan oleh {message.from_user.mention}!"
+            f"⏹️ Obrolan Suara Berakhir/Dihentikan oleh {message.from_user.mention}!"
         )
     if message.command[0][1] == "k":
         if message.chat.id not in db_mem:
@@ -104,7 +100,7 @@ async def admins(_, message: Message):
             await remove_active_chat(chat_id)
             await remove_active_video_chat(chat_id)
             await message.reply_text(
-                "No more music in __Queue__ \n\nLeaving Voice Chat"
+                "Tidak ada lagi musik di__Antrian__ \n\nMeninggalkan Obrolan Suara"
             )
             await stop_stream(chat_id)
             return
@@ -149,7 +145,7 @@ async def admins(_, message: Message):
                 final_output = await message.reply_photo(
                     photo=thumb,
                     reply_markup=InlineKeyboardMarkup(buttons),
-                    caption=f"<b>__Skipped Voice Chat__</b>\n\n🎥<b>__Started Playing:__</b> {title} \n⏳<b>__Duration:__</b> {duration_min} \n👤<b>__Requested by:__ </b> {mention}",
+                    caption=f"<b>__Obrolan Suara Dilewati__</b>\n\n🎥<b>__Mulai Bermain:__</b> {title} \n⏱️<b>__Durasi:__</b> {duration_min} Menit\n👤<b>__Atas Permintaan:__ </b> {mention}",
                 )
                 await start_timer(
                     videoid,
@@ -162,7 +158,7 @@ async def admins(_, message: Message):
                 )
             elif str(finxx) == "s1s":
                 mystic = await message.reply_text(
-                    "Skipped.. Changing to next Video Stream."
+                    "Dilewati.. Mengubah ke Aliran Video berikutnya."
                 )
                 afk = videoid
                 read = (str(videoid)).replace("s1s_", "", 1)
@@ -174,7 +170,7 @@ async def admins(_, message: Message):
                         await skip_video_stream(chat_id, videoid, 720, mystic)
                     except Exception as e:
                         return await mystic.edit(
-                            f"Error while changing video stream.\n\nPossible Reason:- {e}"
+                            f"Kesalahan saat mengubah aliran video.\n\nKemungkinan Alasan:- {e}"
                         )
                     buttons = secondary_markup2("Smex1", message.from_user.id)
                     mention = db_mem[afk]["username"]
@@ -183,7 +179,7 @@ async def admins(_, message: Message):
                         photo="Utils/Telegram.JPEG",
                         reply_markup=InlineKeyboardMarkup(buttons),
                         caption=(
-                            f"<b>__Skipped Video Chat__</b>\n\n👤**__Requested by:__** {mention}"
+                            f"<b>__Obrolan Suara Dilewati__</b>\n\n👤**__Atas Permintaan:__** {mention}"
                         ),
                     )
                     await mystic.delete()
@@ -197,7 +193,7 @@ async def admins(_, message: Message):
                     nrs, ytlink = await get_m3u8(videoid)
                     if nrs == 0:
                         return await mystic.edit(
-                            "Failed to fetch Video Formats.",
+                            "Gagal mengambil Format Video.",
                         )
                     try:
                         await skip_video_stream(
@@ -205,7 +201,7 @@ async def admins(_, message: Message):
                         )
                     except Exception as e:
                         return await mystic.edit(
-                            f"Error while changing video stream.\n\nPossible Reason:- {e}"
+                            f"Kesalahan saat mengubah aliran video.\n\nKemungkinan Alasan:- {e}"
                         )
                     theme = await check_theme(chat_id)
                     c_title = message.chat.title
@@ -223,7 +219,7 @@ async def admins(_, message: Message):
                         photo=thumb,
                         reply_markup=InlineKeyboardMarkup(buttons),
                         caption=(
-                            f"<b>__Skipped Video Chat__</b>\n\n🎥<b>__Started Video Playing:__ </b> [{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n👤**__Requested by:__** {mention}"
+                            f"<b>__Obrolan Suara Dilewati__</b>\n\n🎥<b>__Memulai Pemutaran Video:__ </b> [{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n👤**__Atas Permintaan:__** {mention}"
                         ),
                     )
                     await mystic.delete()
@@ -239,7 +235,7 @@ async def admins(_, message: Message):
                     )
             else:
                 mystic = await message.reply_text(
-                    f"**{MUSIC_BOT_NAME} Playlist Function**\n\n__Downloading Next Music From Playlist....__"
+                    f"**{MUSIC_BOT_NAME} Fungsi Daftar Putar**\n\n__Mengunduh Musik selanjutnya dari Daftar Putar....__"
                 )
                 (
                     title,
@@ -248,7 +244,7 @@ async def admins(_, message: Message):
                     thumbnail,
                 ) = get_yt_info_id(videoid)
                 await mystic.edit(
-                    f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}\n\n0% ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
+                    f"**{MUSIC_BOT_NAME} Mengunduh**\n\n**Judul:** {title[:50]}\n\n0% ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
                 )
                 downloaded_file = await loop.run_in_executor(
                     None, download, videoid, mystic, title
@@ -269,7 +265,7 @@ async def admins(_, message: Message):
                     photo=thumb,
                     reply_markup=InlineKeyboardMarkup(buttons),
                     caption=(
-                        f"<b>__Skipped Voice Chat__</b>\n\n🎥<b>__Started Playing:__ </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n⏳<b>__Duration:__</b> {duration_min} Mins\n👤**__Requested by:__** {mention}"
+                        f"<b>__Obrolan Suara Dilewati__</b>\n\n🎥<b>__Mulai Bermain:__ </b>[{title[:25]}](https://www.youtube.com/watch?v={videoid}) \n⏱️<b>__Durasi:__</b> {duration_min} Menit\n👤**__Atas Permintaan:__** {mention}"
                     ),
                 )
                 os.remove(thumb)
